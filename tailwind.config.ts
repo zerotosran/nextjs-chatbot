0fc50a08b95e154126f9464c439c457eaf4b697a
zerotosran <zerotosran@hotmail.com>
2024-07-29 01:51:01 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,8 @@ module.exports = {
   // ... other configs
   theme: {
     extend: {
-      colors: {
-        beige: '#F5F5DC',
-      },
       fontFamily: {
-        retro: ['VT323', 'monospace'],
+        mono: ['Courier', 'monospace'],
       },
     },
   },
